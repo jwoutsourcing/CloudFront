3117663c88a750ed036b1e2fd28e1df26c87ade4
jwoutsourcing <ec2-user@ip-172-31-52-153.ec2.internal>
2019-05-01 08:30:20 +0000

    Initial commit

---
+++ s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "steve-w-www_distribution" {
+  bucket = "steve-w-www-bucket"
+  acl    = "public-read"
+
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::steve-w-www_distribution/*"]
+    }
+  ]
+}
+POLICY
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+}
