dc38d1c008a3afa5f50f27f8da0523b7e74a68b2
jwoutsourcing <ec2-user@ip-172-31-52-153.ec2.internal>
2019-05-01 10:58:20 +0000

    Lambda stuff

---
+++ lambda.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "ServerlessExample"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "terraform-serverless-example"
+  s3_key    = "v1.0.0/example.zip"
+
+  handler = "main.handler"
+  runtime = "nodejs6.10"
+
+  role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
